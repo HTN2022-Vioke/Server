93dd202ba4aa566d19356750f731f1418efb57bb
Candice Zhang <58599808+candicez03@users.noreply.github.com>
2023-09-24 20:33:21 -0400

    remove test code

--- app/pitch_shift.py
+++ app/pitch_shift.py
@@ -1,16 +1,9 @@
-import datetime # for test code only
-import utils.audio_utils as audio_utils
 import pyrubberband as pyrb
 import soundfile as sf
+import utils.audio_utils as audio_utils
 
 # has to be wav, which is fine since the off vocal is always returned as wav
 def shift_audio(filepath, output_path, shift_semitones):
   data, sample_rate = sf.read(filepath)
   data_shift = pyrb.pitch_shift(data, sample_rate, shift_semitones)
   audio_utils.writeWaveformArrToWav(data_shift, output_path, sample_rate)
-
-# test code
-# start = datetime.datetime.now()
-# shiftAudio("let_it_go_off_vocal.wav", "let_it_go_off_vocal_-2.wav", -2)
-# end = datetime.datetime.now()
-# print("duration: " + str(end-start))
