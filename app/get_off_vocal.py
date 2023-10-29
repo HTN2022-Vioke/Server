3fa2b4b13671cdfd71b9717cbcc2e7d443eafd3b
Candice Zhang <58599808+candicez03@users.noreply.github.com>
2023-10-29 16:56:49 -0400

    change off vocal endpoint to use vocal-remover

--- app/get_off_vocal.py
+++ app/get_off_vocal.py
@@ -1,88 +1,58 @@
-#temp
-import shutil
-import shlex
-# import os
 
-# def createOffVocal(filepath, output_path): # must be wav
-#   shutil.copy(filepath, output_path)
+from audio_separator import Separator
+from pydub import AudioSegment
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+import datetime
+import joblib
+import soundfile as sf
+import utils.audio_utils as audio_utils
+import wave
 
-#TODO: convert everything to mp3
-# TODO: change it to use Demucs api after their 4.1.0 is released
-# import demucs.api 
-
-import subprocess
+SLICE_SIZE_MS = 15 * 1000
+TEMP_DIRECTORY = "temp" # make sure the temp folder exists
+MODEL_NAME = "UVR_MDXNET_KARA_2"
 
 def createOffVocal(filepath, output_path): # must be wav
-  subprocess.run(['python3', '-m', 'demucs', '-n', 'mdx_extra_q', '-d', 'cpu', filepath])
+  f = sf.SoundFile(filepath)
+  sample_rate = f.samplerate
 
-  # separator = demucs.api.Separator(model="mdx_extra")
-  # _, separated = separator.separate_audio_file(filepath)
-  # for file, sources in separated:
-  #   demucs.api.save_audio(sources["vocals"], f"{output_path}_{file}", samplerate=separator.samplerate)
+  audio = AudioSegment.from_file(filepath , "wav")
+  chunks = make_chunks(audio, SLICE_SIZE_MS)
+
+  in_files = joblib.Parallel(n_jobs=8)(\
+    joblib.delayed(processChunk)(i, chunk) for i, chunk in enumerate(chunks))
   
-  # for file, sources in separated:
-  #   for stem, source in sources.items():
-  #     demucs.api.save_audio(source, f"{stem}_{file}", samplerate=separator.samplerate)
-
-  # demucs.separate.main(["--two-stems", "vocals", "-n", "mdx_extra", "--wav", filepath])
-  shutil.copy(filepath, output_path)
-
-        
-# from pydub import AudioSegment
-# from pydub.utils import make_chunks
-# from spleeter.audio.adapter import AudioAdapter
-# from spleeter.separator import Separator
-# import soundfile as sf
-# import wave
-
-# AUDIO_LOADER = AudioAdapter.default()
-# SEPARATOR = Separator('spleeter:2stems')
-# SLICE_SIZE_MS = 30 * 1000
-# TEMP_DIRECTORY = "temp" # make sure the temp folder exists
-
-# def createOffVocal(filepath, output_path): # must be wav
-#   f = sf.SoundFile(filepath)
-#   sample_rate = f.samplerate
-
-#   audio = AudioSegment.from_file(filepath , "wav")
-#   chunks = make_chunks(audio, SLICE_SIZE_MS)
-
-#   in_files = list()
-#   for i, chunk in enumerate(chunks):
-#     chunk_path = "{dir}/chunk{n}.wav".format(dir = TEMP_DIRECTORY, n = i)
-#     chunk.export(chunk_path, format="wav")
-
-#     chunk_path_off_vocal = "{dir}/chunk{n}_off_vocal.wav".format(dir = TEMP_DIRECTORY, n = i)
-#     data = getOffVocalWaveformArr(chunk_path, sample_rate)
-#     writeWaveformArrToWav(data, chunk_path_off_vocal, sample_rate)
-#     in_files.append(chunk_path_off_vocal)
-  
-#   data= []
-#   for in_file in in_files:
-#     w = wave.open(in_file, 'rb')
-#     data.append( [w.getparams(), w.readframes(w.getnframes())] )
-#     w.close()
+  data= []
+  for in_file in in_files:
+    w = wave.open(in_file, 'rb')
+    data.append( [w.getparams(), w.readframes(w.getnframes())] )
+    w.close()
     
-#   output = wave.open(output_path, 'wb')
-#   output.setparams(data[0][0])
-#   for i in range(len(data)):
-#     output.writeframes(data[i][1])
-#   output.close()
+  output = wave.open(output_path, 'wb')
+  output.setparams(data[0][0])
+  for i in range(len(data)):
+    output.writeframes(data[i][1])
+  output.close()
   
   
-# def getOffVocalWaveformArr(filepath, sample_rate): # has to be wav
-#   waveform, _ = AUDIO_LOADER.load(filepath, sample_rate=sample_rate)
-#   prediction = SEPARATOR.separate(waveform)
-#   return prediction["accompaniment"]
+# returns the path to the off vocal wav of the given chunk
+def processChunk(idx, chunk): # has to be wav
+  chunk_path = "{dir}/chunk{idx}.wav".format(dir = TEMP_DIRECTORY, idx = idx)
+  chunk.export(chunk_path, format="wav")
+  separator = Separator(chunk_path, model_name=MODEL_NAME)
+  inst_path, _ = separator.separate()
+  # audio_separator's default saving path
+  # chunk_path_off_vocal = "{dir}/chunk{idx}_(Instrumental)_{model}.wav"\
+  #   .format(dir = TEMP_DIRECTORY, idx = idx, model=MODEL_NAME)
+  return inst_path
+  
 
 # test code
-# start = datetime.datetime.now()
-# # convert to wav
-# sound = AudioSegment.from_mp3("syl.mp3")
-# sound.export("syl.wav", format="wav")
-# createOffVocal("syl.wav", "syl_off_vocal.wav")
-# end = datetime.datetime.now()
-# print("duration: " + str(end-start))
-
-
-  
+start = datetime.datetime.now()
+# convert to wav
+sound = AudioSegment.from_mp3("hc.mp3")
+sound.export("hc.wav", format="wav")
+createOffVocal("hc.wav", "hc_off_vocal.wav")
+end = datetime.datetime.now()
+print("duration: " + str(end-start))
