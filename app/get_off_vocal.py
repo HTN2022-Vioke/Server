5d557a40a215e47d360a682b45a299b1de282911
Candice Zhang <58599808+candicez03@users.noreply.github.com>
2023-10-29 17:36:13 -0400

    comment out test code

--- app/get_off_vocal.py
+++ app/get_off_vocal.py
@@ -1,18 +1,12 @@
 
 from audio_separator import Separator
-from pydub import AudioSegment
-from pydub import AudioSegment
-from pydub.utils import make_chunks
-import datetime
-import soundfile as sf
-import utils.audio_utils as audio_utils
-import wave
 import os 
+# --- test code
+# from pydub import AudioSegment
+# from pydub import AudioSegment
+# import datetime
 
-SLICE_SIZE_MS = 15 * 1000
-TEMP_DIRECTORY = "temp" # make sure the temp folder exists
 MODEL_NAME = "UVR_MDXNET_KARA_2"
-NUM_THREADS = 8
 
 def createOffVocal(filepath, output_path): # must be wav
   separator = Separator(filepath, model_name=MODEL_NAME)
@@ -20,12 +14,12 @@ def createOffVocal(filepath, output_path): # must be wav
   os.remove(vocal_path)
   os.rename(inst_path, output_path)
   
-# test code
-start = datetime.datetime.now()
-song = "hc"
-# convert to wav
-sound = AudioSegment.from_mp3("{song}.mp3".format(song=song))
-sound.export("{song}.wav".format(song=song), format="wav")
-createOffVocal("{song}.wav".format(song=song), "outputs/{song}_off_vocal.wav".format(song=song))
-end = datetime.datetime.now()
-print("duration: " + str(end-start))
+# --- test code
+# start = datetime.datetime.now()
+# song = "hc"
+# # convert to wav
+# sound = AudioSegment.from_mp3("{song}.mp3".format(song=song))
+# sound.export("{song}.wav".format(song=song), format="wav")
+# createOffVocal("{song}.wav".format(song=song), "outputs/{song}_off_vocal.wav".format(song=song))
+# end = datetime.datetime.now()
+# print("duration: " + str(end-start))
