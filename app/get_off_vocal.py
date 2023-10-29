1e3a4f22f4f5efe9fb594d5ffdd0e4ffe4f478da
Candice Zhang <58599808+candicez03@users.noreply.github.com>
2023-10-29 17:46:24 -0400

    nuke test code

--- app/get_off_vocal.py
+++ app/get_off_vocal.py
@@ -1,10 +1,6 @@
 
 from audio_separator import Separator
-import os 
-# --- test code
-# from pydub import AudioSegment
-# from pydub import AudioSegment
-# import datetime
+import os
 
 MODEL_NAME = "UVR_MDXNET_KARA_2"
 
@@ -13,13 +9,4 @@ def createOffVocal(filepath, output_path): # must be wav
   inst_path, vocal_path = separator.separate()
   os.remove(vocal_path)
   os.rename(inst_path, output_path)
-  
-# --- test code
-# start = datetime.datetime.now()
-# song = "hc"
-# # convert to wav
-# sound = AudioSegment.from_mp3("{song}.mp3".format(song=song))
-# sound.export("{song}.wav".format(song=song), format="wav")
-# createOffVocal("{song}.wav".format(song=song), "outputs/{song}_off_vocal.wav".format(song=song))
-# end = datetime.datetime.now()
-# print("duration: " + str(end-start))
+
